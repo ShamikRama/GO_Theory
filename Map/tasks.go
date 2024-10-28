d803887ce5d86effa30e0fc36ebf0e3b1807862d
Sonia Leena <sonialee@ev-g5.kzn.21-school.ru>
2024-10-29 00:42:50 +0300

    добавлены задачи

---
+++ Map/tasks.go
@@ -0,0 +1,27 @@
+// Здесь будут задачи
+
+// 1---------------------------------
+// Дано: два неупорядоченных среза.
+// а) a := []int{37, 5, 1, 2} и b := []int{6, 2, 4, 37}.
+// б) a = []int{1, 1, 1} и b = []int{1, 1, 1, 1}.
+// Верните их пересечение.
+
+func per(a, b []int) (res []int) {
+	counter := map[int]int
+	for _, elem := range a {
+		if counter[elem] == 0 { // проверка существует ли элемент в мапе
+			counter[elem] = 1
+		} else {
+			counter[elem] += 1
+		}
+	}
+	for _, elem := range b {
+		if counter[elem] > 0 { // проверка существует ли элемент в мапе
+			counter[elem] -= 1
+			res = append(res, elem)
+		}
+	}
+	return res
+}
+
+// -------------------------------
