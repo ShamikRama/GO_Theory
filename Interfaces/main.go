afd460cf0fc67d60ccc467083bddf02abb21186b
Sonia Leena <sonialee@si-b3.kzn.21-school.ru>
2024-11-01 18:07:24 +0300

    добавил интерфейсы

---
+++ Interfaces/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type PlayerFootball struct {
+	power int
+}
+
+type PlayerSoccer struct {
+	power int
+}
+
+type P interface {
+	Kick()
+}
+
+func (p *PlayerFootball) Kick(power int) {
+	fmt.Println("Kicking the small ball with power", power)
+}
+
+func (p *PlayerSoccer) Kick(power int) {
+	fmt.Println("Kicking the big ball with power", power)
+}
+
+func main() {
+	player1 := &PlayerFootball{
+		power: 10,
+	}
+	player2 := &PlayerSoccer{
+		power: 11,
+	}
+	player1.Kick(player1.power)
+
+	player2.Kick(player2.power)
+}
