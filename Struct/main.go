81c138d0fe0cbb45f1764c775f2e47dc0955d75c
Sonia Leena <sonialee@si-i6.kzn.21-school.ru>
2024-11-12 21:03:20 +0300

    added

--- Struct/main.go
+++ Struct/main.go
@@ -23,26 +23,62 @@
 // 	fmt.Println(per.Name, per.Age)
 
 // }
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func main() {
+// 	var sum int
+// 	numbers := []int{1, 2, 3, 4, 5}
+// 	var wg sync.WaitGroup
+
+// 	for _, num := range numbers {
+// 		wg.Add(1)
+// 		go func(n int) {
+// 			sum += n
+// 			wg.Done()
+// 		}(num)
+// 	}
+
+// 	wg.Wait()
+// 	fmt.Println("Sum:", sum)
+// }
+
+
+
 package main
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 )
 
-func main() {
-	var sum int
-	numbers := []int{1, 2, 3, 4, 5}
-	var wg sync.WaitGroup
 
-	for _, num := range numbers {
+func Newarray(arr [4]int, new chan<-int){
+	wg := sync.WaitGroup{}
+	for _, val := range arr{
 		wg.Add(1)
-		go func(n int) {
-			sum += n
-			wg.Done()
-		}(num)
-	}
-
+        go func(val int){
+			defer wg.Done()
+            new <- val
+        }(val)
+    }
 	wg.Wait()
-	fmt.Println("Sum:", sum)
+	close(new)
 }
+
+func main(){
+    array := [4]int{2,3,4,5}
+	newchannel := make(chan int, len(array))
+	Newarray(array, newchannel)
+	for val := range newchannel{
+		fmt.Println(val)
+	}
+}
+
--- Struct/struct.go
+++ Struct/struct.go
@@ -1,10 +1,51 @@
-// Структура в golang - это тип данных поле, занчение
-// Это не ссылочный тип данных
-// Когда мы неявно инициализируем структуру, например 
-type Humin struct{}
-h := Human{} // она пустая, но все равно имеет место в памяти, она всегда не nil
-// Проверка на пустоту структуры, можно сделать вот так:
-if h1 == (Human{}) {
-    fmt.Println("h1 is empty")
+// // Структура в golang - это тип данных поле, занчение
+// // Это не ссылочный тип данных
+// // Когда мы неявно инициализируем структуру, например 
+// type Humin struct{}
+// h := Human{} // она пустая, но все равно имеет место в памяти, она всегда не nil
+// // Проверка на пустоту структуры, можно сделать вот так:
+// if h1 == (Human{}) {
+//     fmt.Println("h1 is empty")
+// }
+// // Все остальные проверки h1 == 0 или h1 == nil приведут к ошибке компиляции
+
+// // Это называется встраивание, аналог ООП 
+// type Action struct {
+//     val int
+// }
+
+// func (a Action) Get(){}
+
+// type Human {
+//     Action
+// }
+
+// chel := &Human{}
+
+// chel.Get()
+
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main(){
+    //array := [4]int{2,3,4,5}
+    fmt.Println(newarray(array))
 }
-// Все остальные проверки h1 == 0 или h1 == nil приведут к ошибке компиляции
+
+func newarray(arr []int)[]int{
+    mu := sync.WaitGroup{}
+    newarray := make([]int, 0, len(arr))
+    for _, val := range arr{
+        go func(){
+            mu.Lock()
+            defer mu.Unlock()
+            newarray := append(newarray, val * val)
+        }()
+    }
+    return newarray
+}
+
